27c56245e0cb9c06692a78cc8be78ebaa2f23281
mateuszkitlas <mateusz.kitlas@gmail.com>
2022-01-27 21:14:46 +0100

    rm .pyi file; fixed tree.pyx

--- main/score.py
+++ main/score.py
@@ -1,25 +1,28 @@
-from typing import TypeVar, Generic
+from typing import TypeVar, Generic, Type, Callable
+from .types import RawScore, Timed
 
 T = TypeVar("T")
+U = TypeVar("U")
+R = TypeVar("R")
 
 
 class Score(Generic[T]):
-    def __init__(self, sa, sc, ra, syba):
+    def __init__(self, sa: T, sc: T, ra: T, syba: T):
         self.sa = sa
         self.sc = sc
         self.ra = ra
         self.syba = syba
 
-    def map_with(self, r, score, fn):
-        return Score(
+    def map_with(self, r: Type[R], score: "Score[U]", fn: Callable[[T, U], R]) -> "Score[R]":
+        return Score[R](
             fn(self.sa, score.sa),
             fn(self.sc, score.sc),
             fn(self.ra, score.ra),
             fn(self.syba, score.syba),
         )
 
-    def map(self, fn):
-        return Score(fn(self.sa), fn(self.sc), fn(self.ra), fn(self.syba),)
+    def map(self, fn: Callable[[T], R]) -> "Score[R]":
+        return Score[R](fn(self.sa), fn(self.sc), fn(self.ra), fn(self.syba))
 
     def __str__(self):
         return str(self.json())
@@ -28,5 +31,5 @@ class Score(Generic[T]):
         return vars(self)
 
     @staticmethod
-    def from_raw(raw):
+    def from_raw(raw: RawScore) -> "Score[Timed[float]]":
         return Score(sa=raw["sa"], sc=raw["sc"], ra=raw["ra"], syba=raw["syba"])
