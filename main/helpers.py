5b9476f25ff6faee9011e0591735dac14a7f7028
Mateusz Kitlas <mateusz.kitlas@gmail.com>
2022-10-15 15:47:13 +0200

    - rm askcos - add big files (models for scorers) as data.tz.*

--- main/helpers.py
+++ main/helpers.py
@@ -5,7 +5,7 @@ from typing import List, Literal, Optional, Tuple
 from main.score import Score, Smiles
 from shared import CondaApp
 
-from .types import AcResult, AiInput, AiTree, Timed
+from .types import AiInput, AiTree, Timed
 
 Scoring = Literal["sa", "sc", "ra", "syba", "mf"]
 all_scorings: list[Scoring] = ["sa", "sc", "ra", "syba", "mf"]
@@ -28,26 +28,6 @@ async def app_scorers():
             return Smiles(s, Score(sa=sa, sc=sc, ra=ra, mf=mf, syba=syba), t)
 
         yield f, g
-
-
-@asynccontextmanager
-async def app_ai_ac():
-    ai = CondaApp[AiInput, Timed[AiTree]](4001, "ai", "aizynth-env")
-    ac = CondaApp[str, Timed[AcResult]](4002, "ac", "askcos")
-    try:
-        await asyncio.gather(ai.start(), ac.start())
-        yield (ai.fetch, ac.fetch)
-    finally:
-        try:
-            ai.stop()
-        finally:
-            ac.stop()
-
-
-@asynccontextmanager
-async def app_ac():
-    async with CondaApp[str, Timed[AcResult]](4002, "ac", "askcos") as x:
-        yield x
 
 
 @asynccontextmanager
--- main/main.py
+++ main/main.py
@@ -1,14 +1,12 @@
 import json
-from asyncio import gather, run
+from asyncio import run
 from sys import argv
 from typing import Callable, Coroutine
 
 from shared import Db
 
 from .ai import ai_input_gen
-from .data import data
-from .graph import Graph
-from .helpers import Scoring, all_scorings, app_ac, app_ai, app_scorers
+from .helpers import app_ai, app_scorers
 from .tree import Tree
 from .types import AiTree, Timed
 
@@ -45,54 +43,6 @@ async def ai_postprocess():
 
 
 @use
-async def ac():
-    async with app_ac() as (fetch, _):
-        with db_and_mols() as (db, mol, j, count):
-            print(f"[ac][mol {j}/{count}]")
-            await db.maybe_create(["ac", mol.smiles], lambda: fetch(mol.smiles))
-
-
-@use
-async def score_ac_smileses():
-    mols = data()
-    with Db("db", False) as db:
-        with_nones = (db.read(["ac", mol.smiles]) for mol in mols)
-        graphs = (Graph.from_ac_result(data) for data in with_nones if data)
-        smileses = [smiles for g in graphs for smiles in g.all_smileses()]
-        all = len(smileses)
-        n = 0
-        async with app_scorers() as (scorer, smileser):
-
-            async def f(raw_smiles: str):
-                smiles = await db.read_or_create(
-                    ["smiles", raw_smiles], lambda: smileser(raw_smiles)
-                )
-
-                async def g(scoring: Scoring):
-                    score = await db.read_or_create(
-                        [scoring, raw_smiles], lambda: scorer(scoring, raw_smiles)
-                    )
-                    db.write([scoring, smiles], score)
-
-                await gather(*[g(scoring) for scoring in all_scorings])
-                nonlocal n
-                n += 1
-                print(f"{n}/{all}")
-
-            await gather(*[f(smiles) for smiles in smileses])
-
-
-@use
-async def ac_graph_to_tree():
-    mols = data()
-    with Db("db", False) as db:
-        with_nones = (db.read(["ac", mol.smiles]) for mol in mols)
-        graphs = (Graph.from_ac_result(data) for data in with_nones)
-        for graph in graphs:
-            print(Tree.from_ac(graph, db).stats())
-
-
-@use
 async def cat_db():
     with Db(argv[2], True) as db:
         print(db.as_json())
@@ -115,70 +65,3 @@ if __name__ == "__main__":
         run(method())
     else:
         print(methods.keys())
-
-
-"""
-def ac_tag():
-    mols = data()
-
-    def f(a: int, b: int, c: int):
-        s = float(a + b + c) / 100
-        if s > 0.0:
-            return f"{int(a/s)}%\t{int(b/s)}%\t{int(c/s)}%"
-        else:
-            return f"-\t-\t-"
-
-    with Db() as db:
-        raws = (db.read(["ac", mol.smiles]) for mol in mols)
-        all = [r for r in raws if r]
-        tags = [tag(r) for r in all]
-        tags_pretty = sorted(
-            [
-                (solved, (c["not_solved"], c["internal"], c["solved"]))
-                for _smiles, solved, c in tags
-            ]
-        )
-        solved = sum(solved for _smiles, solved, _counts in tags)
-        print(
-            (
-                "solved?",
-                "not_solved chemicals",
-                "internal chemicals",
-                "solved chemicals",
-            )
-        )
-        for ok, counts in tags_pretty:
-            ok2 = "solved!" if ok else "not solved :("
-            print(f"{ok2}\t{f(*counts)}\t{counts}")
-        print(f"{solved}/{len(all)}")
-
-
-def dummy_scorings():
-    trees = load_trees(json_file)
-    per_mol: dict[str, list[Tuple[Setup, Tree]]] = {
-        ai_input["smiles"]: [] for ai_input, _ in trees
-    }
-    for ai_input, ai_tree in trees:
-        tree = Tree.from_ai_dummy_scorings(ai_tree)
-        per_mol[ai_input["smiles"]].append((ai_input["setup"], tree))
-    for smiles, lst in per_mol.items():
-        print(smiles)
-        for setup, tree in lst:
-            stats = tree.stats()
-            prefix = (
-                "zero_setup"
-                if setup["uw_multiplier"] == 0.0
-                else f"{setup['score']}, n{setup['normalize'][0]}, uw{setup['uw_multiplier']}"
-            )
-            print(
-                f"{prefix} ",
-                {
-                    "internal": stats["internal"]["count"],
-                    "solved": stats["solved"]["count"],
-                    "not_solved": sum(
-                        [s["count"] for s in stats["not_solved"].values()]
-                    ),
-                },
-            )
-
-"""
